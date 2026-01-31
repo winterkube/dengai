4f4cc01f404f1671b10fdb8d5447d9da6cd451ac
winterkube <theicekubex@gmail.com>
2026-01-31 10:18:44 -0800

    pandas

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
+import pandas as pd
+import csv
 
+# with open('train.csv', mode='r', newline='', encoding='utf-8') as file:
+#     reader = csv.reader(file)
+#     for row in reader:
+#         print(row[0])
+#         if (row == 1):
+#             break
+
+df = pd.read_csv("train.csv")
+print(df)
