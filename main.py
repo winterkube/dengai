476dc7377eea944772ca4071fba33fda6458fdb7
winterkube <theicekubex@gmail.com>
2026-01-31 11:58:59 -0800

    work

--- main.py
+++ main.py
@@ -1,12 +1,44 @@
 import pandas as pd
 import csv
-
-# with open('train.csv', mode='r', newline='', encoding='utf-8') as file:
-#     reader = csv.reader(file)
-#     for row in reader:
-#         print(row[0])
-#         if (row == 1):
-#             break
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
 
 df = pd.read_csv("train.csv")
-print(df)
+print("head: \n", df.head())
+print("info: \n", df.info())
+print("is na: \n", df.isna().sum())
+
+df["city"] = df["city"].map({"sj": 0, "iq": 1})
+
+df = df.fillna(df.mean(numeric_only=True))
+
+
+df = df.fillna(0)
+
+df = df.drop(columns=["week_start_date"])
+
+X = df.drop(columns=["total_cases"])
+y = df["total_cases"]
+
+X_train, X_val, y_train, y_val = train_test_split(
+    X, y, test_size=0.2, shuffle=False
+)
+model = RandomForestRegressor(
+    n_estimators=300,
+    max_depth=None,
+    random_state=42,
+    n_jobs=-1
+)
+
+model.fit(X_train, y_train)
+
+df["cases_lag_1"] = df.groupby("city")["total_cases"].shift(1)
+df["cases_lag_2"] = df.groupby("city")["total_cases"].shift(2)
+
+test_df = pd.read_csv("test.csv")
+
+test_df["city"] = test_df["city"].map({"sj": 0, "iq": 1})
+test_df = test_df.drop(columns=["week_start_date"])
+test_df = test_df.fillna(test_df.mean(numeric_only=True))
+
+test_preds = model.predict(test_df)
